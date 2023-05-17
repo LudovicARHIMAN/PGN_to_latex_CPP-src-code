8a58d8151ad4b9948384ca6daf3cdd10fb261196
LudovicARHIMAN <ludovicarhiman900@gmail.com>
2023-05-17 10:58:58 +0400

    tag fix and all game parsing

--- libpgnp/main.cpp
+++ libpgnp/main.cpp
@@ -1,72 +1,150 @@
 #include "PGN.hpp"
 #include <iostream>
+#include <sstream>
+#include <fstream>
 using namespace pgnp;
 
 
 
-
 int main(){
+    
+
     PGN pgn;
     pgn.FromFile("Conv/Adams.pgn");
-    pgn.ParseNextGame();
     
-    
+    while (true)
+    {
 
-    // Recuperer tout les tags 
+        // Parser toutes les parties (et pas une seule partie)
 
-    // Tags requis 
-      
-    std::cout << "Event is: " << pgn.GetTagValue("Event") << std::endl;
-    std::cout << "Site is: " << pgn.GetTagValue("Site") << std::endl;
-    std::cout << "Date is: " << pgn.GetTagValue("Date") << std::endl;
-    std::cout << "Round is: " << pgn.GetTagValue("Round") << std::endl;
-    std::cout << "White is: " << pgn.GetTagValue("White") << std::endl;
-    std::cout << "Black is: " << pgn.GetTagValue("Black") << std::endl;
-    std::cout << "Result is: " << pgn.GetTagValue("Result") << std::endl;
-    
-    //Tags optionnels (existance à Tester)
-    
-    
-    
-    /*
-    std::cout << "WhiteElo is: " << pgn.GetTagValue("WhiteElo") << std::endl;
-    std::cout << "BlackElo is: " << pgn.GetTagValue("BlackElo") << std::endl;
-    std::cout << "ECO is: " << pgn.GetTagValue("ECO") << std::endl;
-    */
+        try
+        {
+            pgn.ParseNextGame();
+        }
+        catch(const NoGameFound& e)
+        {
+            break;
+        }
+        
+        // Recuperer tout les tags obligatoires
 
-
-    // Recuperer les coups de la partie 
-
-    HalfMove *m = new HalfMove();
-    pgn.GetMoves(m);
-    
-    
-    for ( int i = 0; i < m->GetLength() ; i++){
-       std::cout << i << " move is: " << m->GetHalfMoveAt(i)->move << std::endl;
-       
-
-       // Recuperer les commentaires
-        if (!m->GetHalfMoveAt(i)->comment.empty()){
-             std::cout << m->GetHalfMoveAt(i)->comment << std::endl;
-                       
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Event") << std::endl;
         }
 
-    }
-    
-    
-    // Recuperer les variations (à fix)
+        catch(const InvalidTagName& e) {
+            break;
+        }
 
-    for (int v = 0 ; v < m->variations.size() ; v++){
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Site") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Date") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Round") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("White") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Black") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("Result") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+
+        //Tags optionnels 
+
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("WhiteElo") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
         
-        if (m->variations.size() != 0) {
-        std::cout<< "comment" << m->GetHalfMoveAt(v)->variations[v]->comment << std::endl;
-        //std::cout<< "move" << m->GetHalfMoveAt(v)->variations[v]->NAG << std::endl;
-        std::cout<< "NAG" << m->GetHalfMoveAt(v)->variations[v]->move << std::endl;                         
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("BlackElo") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+        
+        try{
+            std::cout << "Event is: " << pgn.GetTagValue("ECO") << std::endl;
+        }
+
+        catch(const InvalidTagName& e) {
+            break;
+        }
+        
+        
+
+        // Recuperer les coups de la partie 
+
+        HalfMove *m = new HalfMove();
+        pgn.GetMoves(m);
+        
+        
+        
+        for ( int i = 0; i < m->GetLength() ; i++){
+        std::cout << i << " move is: " << m->GetHalfMoveAt(i)->move << std::endl;
+        
+
+        // Recuperer les commentaires
+            if (!m->GetHalfMoveAt(i)->comment.empty()){
+                std::cout << m->GetHalfMoveAt(i)->comment << std::endl;
+                        
+            }
+
+        }
+
+        
+
+        
+
+
     }
-
-
-
+    
     
     return(0);
+
+    
 
 }
