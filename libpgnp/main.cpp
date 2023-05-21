c472adff6abb330642b74ab56875641c51de5385
Ludovic ARHIMAN <118427460+LudovicARHIMAN@users.noreply.github.com>
2023-05-21 11:53:44 +0400

    bugs fix, php->cpp exec

--- libpgnp/main.cpp
+++ libpgnp/main.cpp
@@ -1,255 +1,131 @@
-#include "PGN.hpp"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-using namespace pgnp;
+<?php
+    // Vérifie si un fichier a été soumis
+    if (isset($_FILES['pgn_file'])) {
+        $file = $_FILES['pgn_file'];
+        
+        // Vérifie si le fichier a été téléchargé sans erreur
+        if ($file['error'] === UPLOAD_ERR_OK) {
+            $filename = $file['name'];
+            $tmpFilePath = $file['tmp_name'];
+            $name = pathinfo($filename, PATHINFO_FILENAME);
+            
+            // Définir le dossier de destination
+            $destinationFolder = '/var/www/html/Convert/libpgnp/tmp/';
+                        
+            // full path des fichiers qui serra donné en argument au convertisseur
+            
+            $in_path = '/var/www/html/Convert/libpgnp/tmp/'.$name.".pgn"; // path du ficher pgn à convertir
+            $out_path = "/var/www/html/Convert/libpgnp/converted/"."$name".".tex" ; // path du fichier convertis
+            
+            $out_name= $name.".tex"; 
+            $fileUrl = 'http://192.168.1.140/Convert/libpgnp/converted/'.$out_name;
+            
+            // Déplacer le fichier téléchargé vers le dossier de destination
+            $destinationPath = $destinationFolder . $filename;
+            if (move_uploaded_file($tmpFilePath, $destinationPath)) {
+                echo "Le fichier a été téléchargé avec succès.";
+                
+                
+                // Appeler le convertisseur pour développer le fichier en LaTeX 
+                exec("cd /var/www/html/Convert/libpgnp && ./main ".$in_path." ".$out_path); 
+                // le convertisseur fonctione avec des arguments: le path du fichier à convertir, et le path du fichier de sortie
+                
+                
+            
+            } else {
+                echo "Une erreur s'est produite lors du déplacement du fichier.";
+            }
+            
+
+           
+        }    
+    }
 
 
-
-
-int main(){
     
-    std::string outputPath = "/var/www/html/Convert/libpgnp/converted/file.tex"; // Chemin où le fichier sera enregistré
-
-    std::ofstream outfile(outputPath);
-    std::stringstream buffer;
-
-    // Début du document latex
-
-    buffer
-    << "\\documentclass{article}\n"
-    << "\\usepackage{multicol}\n"
-    << "\\usepackage{array}\n"
-    << "\\usepackage{makeidx}\n"
-    << "\\usepackage[skaknew]{chessfss}\n"
-    << "\\usepackage{texmate}\n"
-    << "\\usepackage{xskak}\n"
-    << "\\usepackage[top=1.5cm, bottom=2cm, left=1.5cm, right=1cm,headheight=15pt]{geometry}\n"
-    << "\\usepackage{adjmulticol}\n"
-    << "\\usepackage{ragged2e}\n\n"
-    << "\\begin{document}\n\n";
-
-
+    // Récupérer la liste des fichiers convertis
+    $targetDirectory = '/var/www/html/Convert/libpgnp/converted';
+    $files = scandir($targetDirectory);
+    $files = array_diff($files, array('.', '..'));
     
-    PGN pgn;
-    pgn.FromFile("tmp/hartwig.pgn");
     
-    while (true)
-    {   
-        
-        // Parser toutes les parties (et pas une seule partie)
 
-        try
-        {
-            pgn.ParseNextGame();
-        }
-        catch(const NoGameFound& e)
-        {
-            break;
-        }
-        
+    // permet d'effacer les fichiers une fois la convertion effectué
+    function clean() {
+        exec("rm -rf /var/www/html/Convert/libpgnp/converted/*  && rm -rf /var/www/html/Convert/libpgnp/tmp/*");
+    }
 
-
-        
-
-        
-
-        // Recuperer tout les 7 tags obligatoires
-        
-        try{
-            pgn.STRCheck();
-        }
-
-        catch(const STRCheckFailed& e) {
-            break;
-        }
-        
        
-        buffer
-        << "\\chessevent{" << pgn.GetTagValue("Event") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("Site") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("Date") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("Round") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("White") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("Black") << "}\n"
-        << "\\chessevent{" << pgn.GetTagValue("Result") << "}\n\n";
-
-        
-
-        //Tags optionnels existence à tester 
-        
-        std::string tag_opt; //variable qui servira à tester l'existence des tag optionnels
-
-
-        
-        
-        if (!tag_opt.empty()) {
-
-            buffer << "\\chessevent{" << tag_opt << "}\n";
-
+    // regarde si le dosser destination est vide 
+    function is_empty($targetDirectory) {
+        if (count(glob("$empty/*")) === 0 ) {
+            return FALSE;
         }
-        
-        tag_opt.clear(); // effacer le contenu de la variable pour pouvoir la réutiliser pour tester l'existence des autres tags
-
-
-        // Tester si le tag WhiteElo existe
-        try {
-
-           tag_opt = pgn.GetTagValue("WhiteElo");
-
-        } catch (const InvalidTagName& e) {
-
-            // tag non trouvé donc la variable tag_opt sera vide 
-
-        }
-
-        
-        if (!tag_opt.empty()) {
-
-            buffer << "\\chessevent{" << tag_opt << "}\n";
-
-        }
-
-        // Effacer le contenu de la variable tampon
-        tag_opt.clear();
-        
-         // Tester si le tag BlackElo existe
-        try {
-
-           tag_opt = pgn.GetTagValue("BlackElo");
-
-        } catch (const InvalidTagName& e) {
-
-            // tag non trouvé donc la variable tag_opt sera vide 
-
-        }
-
-        
-        if (!tag_opt.empty()) {
-
-            buffer << "\\chessevent{" << tag_opt << "}\n";
-
-        }
-
-        // Effacer le contenu de la variable tampon
-        tag_opt.clear();
-        
-        // Tester si le tag ECO existe
-        try {
-
-           tag_opt = pgn.GetTagValue("ECO");
-
-        } catch (const InvalidTagName& e) {
-
-            // tag non trouvé donc la variable tag_opt sera vide 
-
-        }
-
-        
-        
-        
-        
-
-        // Recuperer les coups de la partie 
-
-        HalfMove *m = new HalfMove();
-        pgn.GetMoves(m);
-        
-
-        int j = 0;
-        int count = 1;
-
-        for ( int i = 0; i < m->GetLength() ; i++){
-            j++;
-            if (j==1) {
-                
-                buffer
-                << "\\mainline{" << count << ". " ;
-                
-                
-            }
-
-            buffer
-            << m->GetHalfMoveAt(i)->move << " ";
-            
-           
-            
-            if (j != 8) {
-
-
-                if(m->GetHalfMoveAt(i)->isBlack==1){
-                count+=1;
-                
-                buffer
-                << count << ". " ;
-                }   
-
-            }
-
-            if (j==8){
-                
-                buffer
-                << "}\n";
-            }
-
-            
-            if (i==(m->GetLength())-1) {
-                buffer
-                << "}\n";
-            }
-
-
-
-
-                
-            /*
-            std::cout << i << " move is: " << m->GetHalfMoveAt(i)->move << std::endl;
-            */
-            
-            
-            // << "\\mainline{" << i <<"." << m->GetHalfMoveAt(i)->move << "}\n"
-            
-            
-            
-            if (j==8) {
-                
-                buffer
-                << "\\scalebox{0.90}{\\chessboard}\n"; 
-
-
-
-                // Recuperer les commentaires
-                if (!m->GetHalfMoveAt(i)->comment.empty()){
-                    std::cout << m->GetHalfMoveAt(i)->comment << std::endl;
-                            
-                }
-
-                j=0;
-
-
-
-
-            }
-            
-           
-
+        else {
+            return TRUE;
         }
 
     }
 
-    // Fin du document 
-    buffer 
-    << "\\newpage\\end{document}";
+    
 
 
-    outfile << buffer.str();
 
-    return(0);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload PGN</title>
+    <script src="https://cdn.tailwindcss.com/"></script>
+    <scipt src="type.js"></script>
+</head>
+<body class="bg-grey-200">
+
+    <div class="flex justify-center items-center min-h-screen">
+        <form action="" method="POST" enctype="multipart/form-data" class="max-w-md mx-auto p-6 bg-blue rounded-lg shadow-lg">
+            <div class="mb-10">
+                <label for="pgn_file" class="block text-gray-700 font-bold mb-2">Sélectionnez un fichier PGN :</label>
+                <input type="file" name="pgn_file" accept=".pgn" class="border rounded py-2 px-3 bg-gray-100 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                <div>
+                    <?php
+                        if (is_empty($empty)!=TRUE){
+                        echo('<a id="downloadLink" href="#" onclick="downloadFile()">Télécharger le fichier latex </a>');
+                        }
+                        
+                                               
+
+
+
+                    ?>
+                    
+                    
+                </div>
+            
+            </div>
+            <div class="text-center">
+                <input type="submit" value="Upload" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            </div>
+        </form>
+    </div>
+
+    <script>
+    // JavaScript code
+    function downloadFile() {
+        var fileUrl = '<?php echo $fileUrl; ?>'; // Replace with your PHP variable containing the file URL
+        var fileName = '<?php echo $out_name; ?>'; // Replace with your PHP variable containing the desired file name
+
+        var link = document.createElement('a');
+        link.setAttribute('href', fileUrl);
+        link.setAttribute('download', fileName);
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    </script>
 
     
 
-}
+</body>
+</html>
